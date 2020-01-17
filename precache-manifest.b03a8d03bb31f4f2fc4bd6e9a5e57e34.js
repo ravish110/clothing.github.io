735b90ba92341d78e300c5cb5a52e3b11533f903
ravish110 <ravish.abbas94@gmail.com>
2020-01-17 19:23:53 +0530

    Updates

---
+++ precache-manifest.b03a8d03bb31f4f2fc4bd6e9a5e57e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36184ea81fb29e8ffa11a53b1a7c563",
+    "url": "/clothing.github.io/index.html"
+  },
+  {
+    "revision": "f156b43428188dea827a",
+    "url": "/clothing.github.io/static/css/main.924fec1d.chunk.css"
+  },
+  {
+    "revision": "c4543b724e01ea218001",
+    "url": "/clothing.github.io/static/js/2.8f66a8b9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clothing.github.io/static/js/2.8f66a8b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f156b43428188dea827a",
+    "url": "/clothing.github.io/static/js/main.3ec811aa.chunk.js"
+  },
+  {
+    "revision": "644c050568a54d8114f3",
+    "url": "/clothing.github.io/static/js/runtime-main.309706c2.js"
+  }
+]);
